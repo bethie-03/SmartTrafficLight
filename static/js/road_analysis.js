768c1d739bf853eeccab0088d869f3d01434c6ec
bethie-03 <phuongtrang4583@gmail.com>
2024-06-13 18:32:53 +0700

    update

--- static/js/road_analysis.js
+++ static/js/road_analysis.js
@@ -49,51 +49,17 @@ function decreaseMargin(dot, count) {
 var del = document.querySelector('.del');
 var dots = document.querySelector('.dots');
 var Input = document.querySelector('.Input');
-var Output = document.querySelector('.Output');
 var yesclick = true;
 var chooseAnotherFile = document.querySelector('.chooseOtherfiles')
 var buttonfileUpload = document.querySelector('.browse')
-var placeholderText = document.getElementById("placeholderText");
-var pause=false;
 var processing_dots = document.querySelector('.processing_dots')
-var buttonPause =  document.getElementById('Pause')
 var buttonProcessImage = document.getElementById('ProcessImage')
-var buttonProcessVideo = document.getElementById('ProcessVideo')
-var buttonProcessVideoRealtime = document.getElementById('ProcessVideoRealtime')
-var placeholderTextVehicle = document.getElementById("placeholderText3");
 var alt = document.querySelector('.alt')
-var altInput = document.querySelector('.Inputtext')
 var altOutput = document.querySelector('.Outputtext')
 var vehicleRange = document.querySelector('.vehicleRange');
 var text = document.createElement('p')
 text.id = 'persontext'
 
-function updatePlaceholder() {
-    var fileUpload = document.getElementById("file_upload");
-    var selectedOption = document.querySelector('input[name=input_type]:checked');
-
-    if (selectedOption) {
-        switch (selectedOption.value) {
-            case "image":
-                placeholderText.textContent = "Choose an image";
-                fileUpload.accept = "image/*"; 
-                buttonfileUpload.style.display = 'block';
-                break;
-            case "video":
-                placeholderText.textContent = "Choose a video";
-                fileUpload.accept = "video/*"; 
-                buttonfileUpload.style.display = 'block';
-                break;
-            case "video_realtime":
-                placeholderText.textContent = "Choose a video";
-                fileUpload.accept = "video/*"; 
-                buttonfileUpload.style.display = 'block';
-                break;
-        }
-    } else {
-        buttonfileUpload.style.display = 'none';
-    }
-}
 
 function updatePlaceholder2(){
     var selectedOption1 = document.querySelector('input[id=vehicleRange]');
@@ -104,20 +70,14 @@ function updatePlaceholder2(){
 }
 
 function yesClick() {
-    Input.innerHTML = '';
-    Output.innerHTML = '';
+    var InputImage = document.getElementById('InputImage');
+    InputImage.style.display='none';
     yesclick = true;
     buttonProcessImage.style.display='none'
-    buttonProcessVideo.style.display='none'
-    buttonProcessVideoRealtime.style.display='none'
-    buttonPause.style.display='none'
     del.style.display = 'none';
     chooseAnotherFile.style.display='none';
-    buttonfileUpload.style.display='block';
-    placeholderText.style.display='block';
+    buttonfileUpload.style.display='flex';
     alt.style.display='none'
-    Output.style.display='none'
-    Input.style.width= "100%";
 }
 
 function noClick() {
@@ -125,30 +85,12 @@ function noClick() {
     del.style.display = 'none';
     chooseAnotherFile.style.display='block';
     buttonfileUpload.style.display='none';
-    placeholderText.style.display='none';
-}
-
-function pauseVideo(){
-    var placeholderText2 = document.querySelector(".placeholderText2");
-    var VidElement1 = document.getElementById('InputVideo')
-    var VidElement2 = document.getElementById('OutputVideo')
-    if (!pause){
-        VidElement1.play()
-        VidElement2.play()
-        placeholderText2.textContent = "Pause";
-        pause=true;
-    } else{
-        VidElement1.pause()
-        VidElement2.pause()
-        placeholderText2.textContent = "Play";
-        pause=false;
-    }
+    alt.style.display='block';
 }
 
 function checkFile() {
     var images = Input.getElementsByTagName('img');
-    var videos = Input.getElementsByTagName('video');
-    if (images.length > 0 || videos.length > 0) {
+    if (images.length > 0) {
         del.style.display = 'flex';
     } else {
         yesclick = true;
@@ -157,44 +99,37 @@ function checkFile() {
 
 function uploadImage() {
     if (yesclick) {
-        var fileInput = document.getElementById('file_upload')
-        var selectedOption = document.querySelector('input[name=input_type]:checked');
-        var buttonfileUpload = document.querySelector('.browse')
+        var InputImage = document.getElementById('InputImage');
+        var fileInput = document.getElementById('file_upload');
+        var buttonfileUpload = document.querySelector('.browse');
+        var altInput = document.querySelector('.Inputtext');
+
         var reader = new FileReader();
         file = fileInput.files[0]
+
         if (file) {
             reader.onload = function(e) {
-                if (selectedOption.value == 'image'){
-                    var Element = document.createElement('img');
-                    Element.id='InputImage'
-                    Element.title='Default Image'
-                    Element.style.display='block'
-                    buttonProcessImage.style.display='block'
-                } else if (selectedOption.value == 'video' || selectedOption.value == 'video_realtime'){
-                    var Element = document.createElement('video');
-                    Element.id='InputVideo'
-                    Element.title='Default Video'
-                    Element.autoplay=false;
-                    Element.style.display='block'
-                    if (selectedOption.value == 'video'){
-                        buttonProcessVideo.style.display='block'
-                    } else if (selectedOption.value == 'video_realtime') {
-                        buttonProcessVideoRealtime.style.display='block'
-                    }
-                }
-                Element.src = e.target.result
-                Input.appendChild(Element)
+                InputImage.title = 'Before analyse';
+                InputImage.src = e.target.result;
+                InputImage.style.display='block';
+                buttonProcessImage.style.display='block';
             }
             reader.readAsDataURL(file)
         } else {
             alert('Please choose a file!');
         }
+
+        alt.style.display='block';
+        altInput.style.display='block';
         chooseAnotherFile.style.display='block';
         buttonfileUpload.style.display='none';
-        placeholderText.style.display='none';
     } else {
         alert('false');
     }
+}
+
+function ChoseLane() {
+
 }
 
 function processImage() {
@@ -219,9 +154,6 @@ function processImage() {
     })
     .then(data => {
         Element.src = data.result;
-        Output.appendChild(Element);
-        Input.style.width= "50%";
-        Output.style.display='flex';
         alt.style.display = 'flex';
         processing_dots.style.display='none';
         dots.style.display='none';
@@ -232,87 +164,4 @@ function processImage() {
     
     buttonProcessImage.style.display='none'
     
-}
-
-function processVideo() {
-    var vidElement = document.getElementById('InputVideo');
-    var formData = new FormData();
-    formData.append('video', vidElement.src);
-    formData.append('vehicle_cfd', parseInt(placeholderTextVehicle.textContent) / 100);
-    processing_dots.style.display='flex'
-    dots.style.display='flex'
-
-    fetch('/process-video', {
-        method: 'POST',
-        body: formData
-    })
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        var Element = document.createElement('video');
-        Element.id = 'OutputVideo';
-        Element.title = 'Detected Video';
-        Element.type = 'video/mp4';
-        Element.src = data.result;
-        Input.style.width= "50%";
-        Output.appendChild(Element);
-        Output.style.display = 'flex';
-        buttonPause.style.display = 'block';
-        buttonProcessVideo.style.display = 'none';
-        alt.style.display='flex'
-        altInput.innerHTML='Default Video'
-        altOutput.innerHTML='Detected Video'
-        processing_dots.style.display='none';
-        dots.style.display='none';
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-    buttonProcessVideo.style.display='none'
-}
-
-async function processVideoRealtime(){
-    var vidElement = document.getElementById('InputVideo');
-    var formData = new FormData();
-    formData.append('video', vidElement.src);
-    formData.append('vehicle_cfd', parseInt(placeholderTextVehicle.textContent) / 100);
-
-    var Element = document.createElement('img');
-    Element.id = 'OutputVideo';
-    Element.title = 'Detected Video';
-    Input.style.width= "50%";
-    buttonProcessVideoRealtime.style.display = 'none';
-    alt.style.display='flex'
-    altInput.innerHTML='Default Video'
-    altOutput.innerHTML='Detected Video'
-    processing_dots.style.display='flex'
-    dots.style.display='flex'
-
-    const response = await fetch('/process_video_realtime',{
-        method : 'POST',
-        body : formData
-    })
-    vidElement.play()
-    Output.appendChild(Element);
-    Output.style.display = 'flex';
-    
-    const reader = response.body.getReader()
-    const decoder = new TextDecoder('utf-8')
-
-    while (true){
-        const {value, done} = await reader.read();
-        if (done) {
-            console.log(value)
-            processing_dots.style.display='none';
-            dots.style.display='none';
-            break;
-        }
-        const url = decoder.decode(value)
-        console.log(url)
-        Element.src = url
-    }
 }
