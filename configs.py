4de628340aabfda643705708f99f8ec02f74f49e
bethie03 <phuongtrang4583@gmail.com>
2024-07-12 18:46:33 +0700

    update

--- configs.py
+++ configs.py
@@ -7,6 +7,6 @@ POLY_REG_MODEL_PATH = 'source/polynomial_regression_model.pkl'
 SCALER = joblib.load('source/scaler_mlp.pkl')
 MLP_REG_MODEL = joblib.load('source/mlp_regression_model.pkl')
 
-MODEL = torch.hub.load('ultralytics/yolov5', 'custom', MODEL_PATH, device = 'cuda:0', force_reload=True) 
+MODEL = torch.hub.load('ultralytics/yolov5', 'custom', MODEL_PATH, device = 'cpu', force_reload=True) 
 
         
